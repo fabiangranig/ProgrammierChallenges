e2bb4c77c0620e3aee8a35c4f74921e170425e4e
Fabianshrewd <fabiangranig.fg@gmail.com>
2021-11-01 11:12:35 +0100

    Leetcode Aufgaben hinzugefügt
    Bereits vorhandene Leetcode Aufgaben 1. und 3. hinzugefügt.

---
+++ Leetcode/1.Two Sum/Two_Sum.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] zahl = new Int32[2];
+        bool schalter = false;
+        for (int zaehler = 0; zaehler < nums.Length; zaehler++)
+        {
+            for (int zaehler2 = 0; zaehler2 < nums.Length - 1; zaehler2++)
+            {
+                if (nums[zaehler] + nums[zaehler2] == target && zaehler != zaehler2)
+                {
+                    zahl[0] = zaehler;
+                    zahl[1] = zaehler2;
+                }
+            }
+        }
+        return zahl;
+    }
+}
---
+++ Leetcode/3.Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,38 @@
+﻿public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        string endstring = "";
+            int ergebnis = 0;
+
+            if (s.Length == 1)
+            {
+                ergebnis = 1;
+            }
+            else
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (endstring.Contains(s[i]))
+                    {
+                        if (endstring.Length > ergebnis)
+                        {
+                            ergebnis = endstring.Length;
+                        }
+
+                        int zahl = endstring.IndexOf(s[i]);
+                        endstring = endstring.Substring(zahl + 1);
+                        endstring += s[i];
+                    }
+                    else
+                    {
+                        endstring += s[i];
+                    }
+                }
+                if (endstring.Length > ergebnis)
+                {
+                    ergebnis = endstring.Length;
+                }
+            }
+
+            return ergebnis;
+    }
+}
