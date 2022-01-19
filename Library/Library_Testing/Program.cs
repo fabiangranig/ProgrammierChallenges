4b7ca7b5522d551fdb9f1d352e1d357fd533b012
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-01-19 17:01:46 +0100

    Created some things for my new library
    Added the library, some visual studio IDE to test it and an folder where to save the finished nuggets

---
+++ Library/Library_Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Fabian_G_Library;
+
+namespace Library_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fabian_G_Library_Class.Fabian_G_Library_Test();
+        }
+    }
+}
