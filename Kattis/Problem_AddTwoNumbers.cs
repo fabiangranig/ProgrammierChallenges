d7c0d65b81f118c06bdd238b398df763e68f4710
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-02-01 12:52:19 +0100

    Kattis Problem - AddTwoNumbers

---
+++ Kattis/Problem_AddTwoNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            string[] split = line.Split(" ");
+            int  a = Int32.Parse(split[0]); 
+            int b = Int32.Parse(split[1]);
+            Console.WriteLine(a + b);
+        }
+    }
+}
