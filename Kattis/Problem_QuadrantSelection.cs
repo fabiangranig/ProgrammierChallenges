478597e856d8a2d4cacc954df64ae32b7ee57c4d
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-02-01 15:55:35 +0100

    Kattis Problem - Quadrant Selection

---
+++ Kattis/Problem_QuadrantSelection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Runtime: 20ms
+
+        //Read in the lines
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        //if-Condition
+        if(x > 0)
+        {
+            if(y > 0)
+            {
+                //Output the answer
+                Console.WriteLine(1);
+            }
+            if(y < 0)
+            {
+                //Output the answer
+                Console.WriteLine(4);
+            }
+        }
+        else
+        {
+            if(y > 0)
+            {
+                //Output the answer
+                Console.WriteLine(2);
+            }
+
+            if(y < 0)
+            {
+                //Output the answer
+                Console.WriteLine(3);
+            }
+        }
+    }
+}
