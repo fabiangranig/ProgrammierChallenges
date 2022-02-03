bef9385ec15797b2e54b145ac887ff708dc06869
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-02-03 09:21:40 +0100

    Kattis Problem - Two Sum

---
+++ Kattis/Problem_Two_Sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Read in the values
+        string input = Convert.ToString(Console.ReadLine());
+
+        //Split the number in two seperate ´string
+        string[] numbers = input.Split(" ");
+
+        //Add the two numbers togheter
+        int sum = Int32.Parse(numbers[0]) + Int32.Parse(numbers[1]);
+
+        //Output the answer
+        Console.WriteLine(sum);
+    }
+}
