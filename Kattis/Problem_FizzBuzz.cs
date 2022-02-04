e5e9ed1e3473bcd61e3e8299fa99481afb3cc68d
fabiang <fabiangranig.fg@gmail.com>
2022-02-04 03:03:18 -0500

    Modified the Kattis Problem - FizzBuzz

--- Kattis/Problem_FizzBuzz.cs
+++ Kattis/Problem_FizzBuzz.cs
@@ -13,22 +13,28 @@ public class Program
 	//Go through with a for-loop and Output the solution
 	for(int i = 1; i < Int32.Parse(array[2]) + 1; i++)
 	{
-		//When the first number can be divided
-		if(i % Int32.Parse(array[0]) == 0)
-		{
-			Console.WriteLine("Fizz");
-		}
-
-		//When the second number can divided
-		if(i % Int32.Parse(array[1]) == 0)
-		{
-			Console.WriteLine("Buzz");
-		}
-
-		//When both numbers get divided
+		//Check for only one go through
+		bool checker = false;
+		
+		//When both numbers can be divided
 		if(i % Int32.Parse(array[0]) == 0 && i % Int32.Parse(array[1]) == 0)
 		{
 			Console.WriteLine("FizzBuzz");
+			checker = true;
+		}
+		
+		//When the first number can be divided
+		if(i % Int32.Parse(array[0]) == 0 && checker == false)
+		{
+			Console.WriteLine("Fizz");
+			checker = true;
+		}
+
+		//When the second number can divided
+		if(i % Int32.Parse(array[1]) == 0 && checker == false)
+		{
+			Console.WriteLine("Buzz");
+			checker = true;
 		}
 		
 		//If nothing matches just return the number
