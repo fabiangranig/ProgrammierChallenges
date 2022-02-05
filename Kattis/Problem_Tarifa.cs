5178118176aa45ee8af66f78712e874e3ed836cb
fabiang <fabiangranig.fg@gmail.com>
2022-02-05 08:20:00 -0500

    Added Kattis: Problem_Tarifa

--- Kattis/Problem_Tarifa.cs
+++ Kattis/Problem_Tarifa.cs
@@ -8,33 +8,34 @@ public class Program
 	    //N = how many months there are
 	    //Pi = used in each month
 	    
-	    //Read in all the values
-	    int i = 0;
-	    int X = 0;
-	    int N = 0;
-	    int[] array;
-	    while(Console.ReadLine() != null)
+	    //Get the first two lines
+	    int X = Int32.Parse(Console.ReadLine());
+	    int N = Int32.Parse(Console.ReadLine());
+	    int[] array = new Int32[N];	    
+
+	    //Get the Rest of the lines
+	    int o = 0;
+	    while(o <  N)
 	    {
-		//Read in the first and the second line
-		if(i == 0)
-		{
-			X = Convert.ToInt32(Console.ReadLine());
-		}
-
-		if(i == 1)
-		{
-			N = Convert.ToInt32(Console.ReadLine());
-			array = new Int32[N];
-		}
-
-		//Read in the last of the line
-		if(i > 1)
-		{
-			array[i] = Convert.ToInt32(Console.ReadLine());
-		}
-
-		//Set the counter + 1
-		i++;
+		array[o] = Int32.Parse(Console.ReadLine());
+		o++;
 	    }
+
+	    //Calculation
+	    //Calculate the max of the bandwith
+	    int max_bandwith = X * (N +1);
+
+	    //Calculate the needed bandwith
+	    int needed_bandwith = 0;
+	    for(int i = 0; i < array.Length; i++)
+	    {
+		    needed_bandwith += array[i];
+	    }
+
+	    //Calculate the rest
+	    int solution = max_bandwith - needed_bandwith;
+
+	    //Output the solution
+	    Console.WriteLine(solution);
     }
 }
