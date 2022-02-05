0511f8786643da9ba08ed1ba5e1aac1e629864c7
fabiang <fabiangranig.fg@gmail.com>
2022-02-05 04:21:22 -0500

    First Part of the Tarifa Problem

---
+++ Kattis/Problem_Tarifa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+	    //X = how many megabytes he has each months
+	    //N = how many months there are
+	    //Pi = used in each month
+	    
+	    //Read in all the values
+	    int i = 0;
+	    int X = 0;
+	    int N = 0;
+	    int[] array;
+	    while(Console.ReadLine() != null)
+	    {
+		//Read in the first and the second line
+		if(i == 0)
+		{
+			X = Convert.ToInt32(Console.ReadLine());
+		}
+
+		if(i == 1)
+		{
+			N = Convert.ToInt32(Console.ReadLine());
+			array = new Int32[N];
+		}
+
+		//Read in the last of the line
+		if(i > 1)
+		{
+			array[i] = Convert.ToInt32(Console.ReadLine());
+		}
+
+		//Set the counter + 1
+		i++;
+	    }
+    }
+}
