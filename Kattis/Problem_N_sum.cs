d0fd1f60d630c6644fea941c89a0860223cd74d3
fabiang <fabiangranig.fg@gmail.com>
2022-02-10 04:19:46 -0500

    Kattis Problem - N_Sum

---
+++ Kattis/Problem_N_sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the useless input
+	string u_input = Convert.ToString(Console.ReadLine());
+
+	//Get the usefull input
+	string input = Convert.ToString(Console.ReadLine());
+
+	//Split the input into an array
+	string[] array = input.Split(" ");
+
+	//Sum all the items up
+	int sum = 0;
+	for(int i = 0; i < array.Length; i++)
+	{
+		sum += Int32.Parse(array[i]);
+	}
+
+	//Output the solution
+	Console.WriteLine(sum);
+    }
+}
