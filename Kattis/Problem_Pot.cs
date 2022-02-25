f2328b38f222a11ba0163fc92e4cfba77a725000
fabiang <fabiangranig.fg@gmail.com>
2022-02-25 06:47:04 -0500

    Startet working on Kattis: Pot Problem

---
+++ Kattis/Problem_Pot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+	//Get the lines
+	int input = Convert.ToInt32(Console.ReadLine());
+
+	//Get the numbers
+	string[] in_text = new Int32[input];
+	for(int i = 0; i < input; i++)
+	{
+		//Get the 
+	}
+    }
+}
