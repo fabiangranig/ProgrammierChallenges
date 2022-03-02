7083e33a0c03a84a80fb6151a91bc385c63b2fab
fabiang <fabiangranig.fg@gmail.com>
2022-03-02 04:23:03 -0500

    Startet working on Kattis Problem: Electrical Outlets

---
+++ Kattis/Problem_ElectricalOutlets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+	//Get the n input lines
+	int n_lines = Convert.ToInt32(Console.ReadLine());
+
+	//Get the rest of the lines
+	string[] input = new String[n_lines];
+	for(int i = 0; i < n_lines; i++)
+	{
+		//Put the lines into the array
+		input[i] = Convert.ToString(Console.ReadLine());
+	}
+
+	//Split the lines into single values
+	string[] array = input.Split(" ");
+
+	//Convert the whole array into integers
+	int[] numbers = new Int32[array.Length];
+	for(int i = 0; i < array.Length; i++)
+	{
+		//Convert to Int
+		numbers[i] = Parse.Int32(array[i]);
+	}
+
+	for(int i = 0; i < numbers.Length; i++)
+	{
+		
+	}
+    }
+}
