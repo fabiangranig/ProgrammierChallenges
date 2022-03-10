e9d564a2dd94f36edf2f5b65851f151f249f5687
fabiang <fabiangranig.fg@gmail.com>
2022-03-10 14:03:03 -0500

    Added Kattis Problem: FindingAnA

---
+++ Kattis/Problem_FindingAnA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+	    //Get the string
+	    string o = Convert.ToString(Console.ReadLine());
+
+	    //Check where the first a is and then build the rest of the string
+	    bool s = false;
+	    string e = "";
+	    for(int i = 0; i < o.Length; i++)
+	    {
+	    	if(o[i] == 'a' || o[i] == 'A')
+		{
+			s = true;
+		}
+
+		if(s == true)
+		{
+			e += Convert.ToString(o[i]);
+		}
+	    }
+
+	    //Output the solution
+	    Console.WriteLine(e);
+    }
+}
