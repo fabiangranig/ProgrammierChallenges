ff5e835480af887659baeec123f2d582ae3346a9
fabiang <fabiangranig.fg@gmail.com>
2022-03-17 14:58:32 -0400

    Testing Tab second time

--- Kattis/Problem_FindingAnA.cs
+++ Kattis/Problem_FindingAnA.cs
@@ -4,26 +4,26 @@ public class Program
 {
     public static void Main()
     {
-	    //Get the string
-	    string o = Convert.ToString(Console.ReadLine());
+        //Get the string
+        string o = Convert.ToString(Console.ReadLine());
 
-	    //Check where the first a is and then build the rest of the string
-	    bool s = false;
-	    string e = "";
-	    for(int i = 0; i < o.Length; i++)
-	    {
-	    	if(o[i] == 'a' || o[i] == 'A')
-		{
-			s = true;
-		}
+        //Check where the first a is and then build the rest of the string
+        bool s = false;
+        string e = "";
+        for(int i = 0; i < o.Length; i++)
+        {
+            if(o[i] == 'a' || o[i] == 'A')
+            {
+                s = true;
+            }
 
-		if(s == true)
-		{
-			e += Convert.ToString(o[i]);
-		}
+            if(s == true)
+            {
+                e += Convert.ToString(o[i]);
+            }
 	    }
 
-	    //Output the solution
-	    Console.WriteLine(e);
+        //Output the solution
+        Console.WriteLine(e);
     }
 }
