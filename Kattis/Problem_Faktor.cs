647beb1172b1307b65e15b83ac89da17f8c37985
fabiang <fabiangranig.fg@gmail.com>
2022-03-17 14:39:25 -0400

    Test tab Problem

--- Kattis/Problem_Faktor.cs
+++ Kattis/Problem_Faktor.cs
@@ -2,19 +2,19 @@
 
 public class Program
 {
-    	public static void Main()
-    	{
-		//Get the input
-		string o = Convert.ToString(Console.ReadLine());
+    public static void Main()
+    {
+        //Get the input
+        string o = Convert.ToString(Console.ReadLine());
 
-		//Split the string
-		string[] array_string = o.Split(" ");
+        //Split the string
+        string[] array_string = o.Split(" ");
 
-		//Convert the string
-		int a = Int32.Parse(array_string[0]);
-		int b = Int32.Parse(array_string[1]);
+        //Convert the string
+        int a = Int32.Parse(array_string[0]);
+        int b = Int32.Parse(array_string[1]);
 
-		//Output the solution
-		Console.WriteLine(a * (b - 1) + 1);	
-	}
+        //Output the solution
+        Console.WriteLine(a * (b - 1) + 1);	
+    }
 }
