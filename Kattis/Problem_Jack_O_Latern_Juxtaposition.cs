9eedbcebb3d0086c0dde65952d1f6142e3adbfaf
fabiang <fabiangranig.fg@gmail.com>
2022-03-22 03:51:30 -0400

    Added Kattis Problem: Jack O Latern Juxtaposition

---
+++ Kattis/Problem_Jack_O_Latern_Juxtaposition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Input the line
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Split the lines
+        string[] u = o.Split(" ");
+        
+        //Get the solution
+        int sol = 1;
+        for(int i = 0; i < u.Length; i++)
+        {
+            //Multiply togehter
+            sol *= Int32.Parse(u[i]);
+        }
+
+        //Return the solution
+        Console.WriteLine(sol);
+    }
+}
