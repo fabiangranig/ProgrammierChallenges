c73dd405e88c53a2c6e888c2a1ab9c7d4f86f785
fabiang <fabiangranig.fg@gmail.com>
2022-03-22 04:06:43 -0400

    Adding Kattis Problem: R2

---
+++ Kattis/Problem_R2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Read in the input
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Split the numbers
+        string[] u = o.Split(" ");
+
+        //Calculate the solution
+        int sol = 2 * Int32.Parse(u[1]) - Int32.Parse(u[0]);
+
+        //Output the solution
+        Console.WriteLine(sol);
+    }
+}
