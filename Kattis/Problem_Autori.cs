6998efcade1ab09ea48f418d07707d2cc11c593f
fabiang <fabiangranig.fg@gmail.com>
2022-03-23 14:15:53 -0400

    Added Kattis Problem: Autori

---
+++ Kattis/Problem_Autori.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the input
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Split the lines
+        string[] u = o.Split("-");
+
+        //Get the new string
+        string sol = "";
+        for(int i = 0; i < u.Length; i++)
+        {
+            //Get the first character
+            sol += Convert.ToString(u[i][0]);
+        }
+
+        //Output the solution
+        Console.WriteLine(sol);
+    }
+}
