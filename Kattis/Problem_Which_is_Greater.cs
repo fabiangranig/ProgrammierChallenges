ee1dbf806623df5439a08e3f01262c80e1ceb74d
fabiang <fabiangranig.fg@gmail.com>
2022-03-23 14:20:55 -0400

    Added Kattis Problem: Which is Greater?

---
+++ Kattis/Problem_Which_is_Greater.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the input
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Split the line
+        string[] k = o.Split(" ");
+
+        //Output the solution
+        if(Int32.Parse(k[0]) > Int32.Parse(k[1]))
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+}
