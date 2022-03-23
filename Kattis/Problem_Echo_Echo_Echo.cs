03b0856afe36fe637702d49693518c7c47384ca9
fabiang <fabiangranig.fg@gmail.com>
2022-03-23 14:07:08 -0400

    Added Kattis Problem: Echo Echo Echo

---
+++ Kattis/Problem_Echo_Echo_Echo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the line
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Output string with spaces between
+        Console.WriteLine(o + " " + o + " " + o);
+    }
+}
