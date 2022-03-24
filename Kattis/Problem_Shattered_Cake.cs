68d81e172dc337f5ec4d826563d2883bdee38356
fabiang <fabiangranig.fg@gmail.com>
2022-03-24 08:03:04 -0400

    Added Kattis Problem: Shattered Cake and TriangleArea

---
+++ Kattis/Problem_Shattered_Cake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the input
+        int W = Int32.Parse(Convert.ToString(Console.ReadLine()));
+        
+        //Get the rest of the input and calculate the surface
+        int n = Int32.Parse(Convert.ToString(Console.ReadLine()));
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            string o = Convert.ToString(Console.ReadLine());
+            string[] u = o.Split(" ");
+            sum += (Int32.Parse(u[0]) * Int32.Parse(u[1]));
+        }
+
+        //Divide to get the solution
+        int L = sum / W;
+        
+        //Output the solution
+        Console.WriteLine(L);
+    }
+}
--- Kattis/Problem_TriangleArea.cs
+++ Kattis/Problem_TriangleArea.cs
@@ -4,6 +4,16 @@ public class Program
 {
     public static void Main()
     {
-	
+	    //Get the input
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Split the line
+        string[] u = o.Split(" ");
+
+        //Get the solution
+        double sol = (Convert.ToDouble(Int32.Parse(u[0])) * Convert.ToDouble(Int32.Parse(u[1]))) / 2.0;
+
+        //Output the solution
+        Console.WriteLine(sol);
     }
 }
