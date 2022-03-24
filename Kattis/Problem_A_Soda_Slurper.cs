452cbf2a414053d1467326407ffa4228aa2f200f
fabiang <fabiangranig.fg@gmail.com>
2022-03-24 07:15:22 -0400

    Added Kattis Problem: A Soda Slurper

---
+++ Kattis/Problem_A_Soda_Slurper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the input
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Split the string
+        string[] u = o.Split(" ");
+
+        //Calculate the number of bottles
+        int e = Int32.Parse(u[0]) + Int32.Parse(u[1]);
+        int c = Int32.Parse(u[2]);
+        int b = 0;        
+
+        //Durchgehen mit While
+        while(e - c >= 0)
+        {
+            b++;
+            e -= c;
+            e++;
+        }
+
+        Console.WriteLine(b);
+    }
+}
