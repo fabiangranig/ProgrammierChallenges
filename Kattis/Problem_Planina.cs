9c1f5420c5ebdac92ec5abf6c2a03c375c354609
fabiang <fabiangranig.fg@gmail.com>
2022-03-26 15:00:03 -0400

    Added Kattis Problem: Planina

---
+++ Kattis/Problem_Planina.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the input
+        int o = Int32.Parse(Convert.ToString(Console.ReadLine()));
+
+        //Calculate the solution
+        double u = Math.Pow(2, o) + 1;
+        
+        //Output the solution
+        Console.WriteLine(u * u);
+    }
+}
