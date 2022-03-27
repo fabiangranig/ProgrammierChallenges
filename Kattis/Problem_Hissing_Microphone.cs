8cca36b57828d92c27c629e41ea0c133b6a17a44
fabiang <fabiangranig.fg@gmail.com>
2022-03-27 12:43:14 -0400

    Added Kattis Problem: Hissing Microphone

---
+++ Kattis/Problem_Hissing_Microphone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the input string
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Check if there are two "s" behind each other
+        bool switch1 = false;
+        for(int i = 1; i < o.Length; i++)
+        {
+            if(o[i - 1] == o[i] && o[i] == 's')
+            {
+                switch1 = true;
+                break;
+            }
+        }
+
+        //Output the solution
+        if(switch1 == true)
+        {
+            Console.WriteLine("hiss");
+        }
+        else
+        {
+            Console.WriteLine("no hiss");
+        }
+    }
+}
