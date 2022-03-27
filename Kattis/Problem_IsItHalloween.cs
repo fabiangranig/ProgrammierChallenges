3e71ff1238c96efb17784b907e0b00a99404ba9a
fabiang <fabiangranig.fg@gmail.com>
2022-03-27 12:32:20 -0400

    Added Kattis Problem: IsItHalloween

---
+++ Kattis/Problem_IsItHalloween.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the input
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Output "yup" at the given dates and everything else should show "nope"
+        if(o == "OCT 31" || o == "DEC 25")
+        {
+            //Output yup
+            Console.WriteLine("yup");
+        }
+        else
+        {
+            //Output nope
+            Console.WriteLine("nope");
+        }
+    }
+}
