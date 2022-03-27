e94e97543499e1c3dfd84e5ffab5b4d7cfda48b5
fabiang <fabiangranig.fg@gmail.com>
2022-03-27 07:05:28 -0400

    Added Kattis Problem: Jumbo Javelin

---
+++ Kattis/Problem_Jumbo_Javelin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the number of n lines
+        int n = Int32.Parse(Convert.ToString(Console.ReadLine()));
+
+        //Read in the rest of the numbers and calculate the length
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            sum += Int32.Parse(Convert.ToString(Console.ReadLine()));
+        }
+
+        //Remove the engineer fails because of bad fusing
+        sum -= (n - 1);
+
+        //Output the solution
+        Console.WriteLine(sum);
+    }
+}
