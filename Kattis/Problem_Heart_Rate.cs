463e7b73c3bd3975d84b22dea9d7f862092d1ea2
fabiang <fabiangranig.fg@gmail.com>
2022-03-28 01:19:07 -0400

    Added Kattis Problem: Heart Rate

---
+++ Kattis/Problem_Heart_Rate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the n lines
+        int n = Int32.Parse(Convert.ToString(Console.ReadLine()));
+
+        //Get the rest of the input
+        string[] k = new String[n];
+        for(int i = 0; i < n; i++)
+        {
+            k[i] = Convert.ToString(Console.ReadLine());
+            string[] l = k[i].Split(" ");
+            
+            //Convert to double variables
+            double zahl1 = Convert.ToDouble(l[0]);
+            double zahl2 = Convert.ToDouble(l[1]);
+            
+            //Get the mid heart rate
+            double mid = 60 * zahl1 / zahl2;
+            double temp = 60 / zahl2;
+            double low = mid - temp;
+            double high = mid + temp;
+
+            //Output in array
+            k[i] = Math.Round(low, 4) + " " + Math.Round(mid, 4) + " " + Math.Round(high, 4);
+        }
+
+        //Output the solution into console
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine(k[i]);
+        }
+    }
+}
