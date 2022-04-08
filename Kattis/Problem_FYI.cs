cbbc20f14a2577c82b1dbb0459c7fd301ea90dbc
fabiang <fabiangranig.fg@gmail.com>
2022-04-08 02:38:29 -0400

    Added Kattis Problem: FYI

---
+++ Kattis/Problem_FYI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    //Check the first three characters, if they are "555"
+    static bool first3(string o)
+    {
+        //Go through with an for-loop
+        bool switch1 = true;
+        for(int i = 0; i < 3; i++)
+        {
+            if(o[i] != '5')
+            {
+                switch1 = false;
+            }
+        }
+
+        //Return the solution
+        return switch1;
+    }
+
+    public static void Main()
+    {
+        //Get the input
+        string u = Convert.ToString(Console.ReadLine());
+
+        //Check what Output should be given out
+        bool sol = first3(u);
+
+        //Output the solution
+        if(sol == true)
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+}
