354dc3e41dcaa5847d00894592d0f78939c5ed64
fabiang <fabiangranig.fg@gmail.com>
2022-04-09 02:43:44 -0400

    Added Kattis Problem: Filip

---
+++ Kattis/Problem_Filip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    static string Reversing(string h)
+    {
+        //Go through with for loop backwards
+        string sol = "";
+        for(int i = 0; i < h.Length; i++)
+        {
+            sol += Convert.ToString(h[h.Length - 1 - i]);
+        }
+        
+        //Return the solution
+        return sol;
+    }
+
+    public static void Main()
+    {
+        //Get the input
+        string o = Convert.ToString(Console.ReadLine());
+
+        //Split the input
+        string[] u = o.Split(" ");
+
+        //Reverse the two numbers
+        int n1 = Int32.Parse(Reversing(u[0]));
+        int n2 = Int32.Parse(Reversing(u[1]));
+
+        //Output the larger number
+        if(n1 > n2)
+        {
+            Console.WriteLine(n1);
+        }
+        else
+        {
+            Console.WriteLine(n2);
+        }
+    }
+}
