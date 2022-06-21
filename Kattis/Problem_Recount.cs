6c6ad2d82a38b0eab66b4c24108ed1b3437507ad
fabiang <fabiangranig.fg@gmail.com>
2022-06-21 05:45:02 -0400

    First steps into Kattis Problem: Count

--- Kattis/Problem_Recount.cs
+++ Kattis/Problem_Recount.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Program
 {
     public static void Main()
     {
         //Get the name and sort them in
+        List<string> name = new List<string>();
+        List<int> number = new List<int>();
         string current_name = Convert.ToString(Console.ReadLine());
+        bool schalter;
         while(current_name != ***)
         {
+            //Schalter always on false on start
+            schalter = false;
+
+            //Check if there is already a value
+            for(int i = 0; i < name.Count; i++)
+            {
+                if(name[i] == current_name)
+                {
+                    
+                }
+            }
             
             //Get the next name
             current_name = Convert.ToString(Console.ReadLine());
