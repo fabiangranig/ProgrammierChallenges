39f4592c42ff1d3a5637c8f1087ac2fc1667c730
fabiang <fabiangranig.fg@gmail.com>
2022-06-22 04:25:57 -0400

    Finished problem but there is a problem with the Output. Maybe a value which wont get overwritten.

--- Kattis/Problem_AboveAverage.cs
+++ Kattis/Problem_AboveAverage.cs
@@ -7,6 +7,48 @@ public class Program
         //Get the input
         int set_length = Int32.Parse(Convert.ToString(Console.ReadLine()));
 
-        //Get th
+        //Get the sets
+        string[] sets = new String[set_length];
+        for(int i = 0; i < sets.Length; i++)
+        {
+            sets[i] = Convert.ToString(Console.ReadLine());
+        }
+
+        //Create a solution array and get the solution
+        for(int i = 0; i < sets.Length; i++)
+        {
+            //Get all the numbers out of the set
+            string set = sets[i];
+            string[] set_split = set.Split(" ");
+            int[] arr = new Int32[set_split.Length];
+            for(int u = 0; u < set_split.Length; u++)
+            {
+                arr[u] = Int32.Parse(set_split[u]);
+            }
+
+            //Get the Average
+            double sum = 0;
+            for(int u = 1; u < arr.Length; u++)
+            {
+                sum += arr[u];
+            }
+            double average = sum / Convert.ToDouble(arr[0]);
+            
+            //Go through an check how many are above average
+            int count = 0;
+            for(int u = 0; u < arr.Length; u++)
+            {
+                if(average < arr[u])
+                {
+                    count++;
+                }
+            }
+
+            //Get the percent of how many
+            double sol = Convert.ToDouble(count) / arr[i] * 100;
+
+            //Output the solution
+            Console.WriteLine(Math.Round(sol, 3) + "%");
+        }
     }
 }
