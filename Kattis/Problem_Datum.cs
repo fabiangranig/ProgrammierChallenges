ca5641bd4048865dbbb7e727fd0f37a16288fd1f
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-10-05 20:16:22 +0200

    Started working on Kattis Problem: Datum

---
+++ Kattis/Problem_Datum.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Get the the days each month
+        int[] days_each_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        //Get all days
+        string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        
+    }
+}
