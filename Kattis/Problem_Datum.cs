e2e9fd9fe6af6fd02f3cb0a9fbf055da6159006a
Fabianshrewd <fabiangranig.fg@gmail.com>
2022-10-06 10:02:05 +0200

    Added Sum of days to the problem

--- Kattis/Problem_Datum.cs
+++ Kattis/Problem_Datum.cs
@@ -2,6 +2,11 @@ using System;
 
 public class Program
 {
+    static int PassThrough(int sum)
+    {
+        
+    }
+
     public static void Main()
     {
         //Get the the days each month
@@ -9,7 +14,24 @@ public class Program
 
         //Get all days
         string[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int starting_index = 3;
 
+        //Get the input and split the input
+        string input = Convert.ToString(Console.ReadLine());
+        string[] split = input.Split(" ");
+        int day = Int32.Parse(split[0]);
+        int month = Int32.Parse(split[1]);
+
+        //Get the sum of the days before that date
+        int sum = 0;
+        for(int i = 0; i < month - 1; i++)
+        {
+            sum += days_each_month[i];
+        }
+
+        //Add the days of this month
+        sum += day;
+        
         
     }
 }
