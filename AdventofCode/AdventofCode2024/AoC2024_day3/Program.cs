1612a0736d010848b141aa7d25aeda335f98b6ac
fabiangranig <fabiangranig.fg@gmail.com>
2024-12-04 14:48:18 +0000

    Added AoC2024 day 3 and day 4

---
+++ AdventofCode/AdventofCode2024/AoC2024_day3/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024_day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int part1_solution = Part1(input);
+            Console.WriteLine("This solution to part 1 is: " + part1_solution);
+
+            int part2_solution = Part2(input);
+            Console.WriteLine("This solution to part 2 is: " + part2_solution);
+        }
+
+        static int Part1(string[] input)
+        {
+            int sum = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                string line = input[i];
+                sum += SearchForProduct(line);
+            }
+            return sum;
+        }
+
+        static int Part2(string[] input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                string line = input[i];
+                sum += SearchForProduct2(line);
+            }
+            return sum;
+        }
+
+        static int SearchForProduct(string line)
+        {
+            int sum = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (i + 4 < line.Length && line[i] == 'm' && line[i + 1] == 'u' && line[i + 2] == 'l' && line[i + 3] == '(')
+                {
+                    string StartOfSearchedValue = line.Substring(i + 4);
+                    string[] split1 = StartOfSearchedValue.Split(',');
+                    string[] split2 = split1[1].Split(')');
+                    int number1;
+                    bool case1 = Int32.TryParse(split1[0], out number1);
+                    int number2;
+                    bool case2 = Int32.TryParse(split2[0], out number2);
+
+                    if(case1 && case2)
+                    {
+                        sum += number1 * number2;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        static bool enabled = true;
+
+        static int SearchForProduct2(string line)
+        {
+            int sum = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if(i + 4 < line.Length && line.Substring(i, 4) == "do()")
+                {
+                    enabled = true;
+                }
+
+                if(i + 7 < line.Length && line.Substring(i, 7) == "don't()")
+                {
+                    enabled = false;
+                }
+
+                if (i + 4 < line.Length && line[i] == 'm' && line[i + 1] == 'u' && line[i + 2] == 'l' && line[i + 3] == '(')
+                {
+                    string StartOfSearchedValue = line.Substring(i + 4);
+                    string[] split1 = StartOfSearchedValue.Split(',');
+                    string[] split2 = split1[1].Split(')');
+                    int number1;
+                    bool case1 = Int32.TryParse(split1[0], out number1);
+                    int number2;
+                    bool case2 = Int32.TryParse(split2[0], out number2);
+
+                    if (case1 && case2 && enabled)
+                    {
+                        sum += number1 * number2;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ AdventofCode/AdventofCode2024/AoC2024_day4/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024_day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int part1_solution = Part1(input);
+            Console.WriteLine("The solution to part one is: " + part1_solution);
+
+            int part2_solution = Part2(input);
+            Console.WriteLine("The solution to part two is: " + part2_solution);
+        }
+
+        static int Part1(string[] input)
+        {
+            char[,] chars = BuildCharArray(input);
+            return GetAllWords(chars);
+        }
+        static int Part2(string[] input)
+        {
+            char[,] chars = BuildCharArray(input);
+            return GetAllWords2(chars);
+        }
+
+        static int GetAllWords(char[,] chars)
+        {
+            int sum = 0;
+            for(int i = 0; i < chars.GetLength(0); i++)
+            {
+                for(int u = 0; u < chars.GetLength(1); u++)
+                {
+                    if (chars[i, u] == 'X')
+                    {
+                        sum += CheckFromThatPositionIfWordsExist(chars, i, u);
+                    }
+                }
+            }
+            return sum;
+        }
+        static int GetAllWords2(char[,] chars)
+        {
+            int sum = 0;
+            for (int i = 0; i < chars.GetLength(0); i++)
+            {
+                for (int u = 0; u < chars.GetLength(1); u++)
+                {
+                    if (chars[i, u] == 'A')
+                    {
+                        sum += CheckFromThatPositionIfWordsExist2(chars, i, u);
+                    }
+                }
+            }
+            return sum;
+        }
+
+        static int CheckFromThatPositionIfWordsExist(char[,] chars, int i, int u)
+        {
+            int counter = 0;
+
+            //Horizontal
+            if(u + 3 < chars.GetLength(1) && chars[i, u + 1] == 'M' && chars[i, u + 2] == 'A' && chars[i, u + 3] == 'S')
+            {
+                counter++;
+            }
+
+            //Horizontal backwards
+            if(u - 3 > -1 && chars[i, u - 1] == 'M' && chars[i, u - 2] == 'A' && chars[i, u - 3] == 'S')
+            {
+                counter++;
+            }
+
+            //Vertical
+            if(i + 3 < chars.GetLength(0) && chars[i + 1, u] == 'M' && chars[i + 2, u] == 'A' && chars[i + 3, u] == 'S')
+            {
+                counter++;
+            }
+
+            //Vertical backwards
+            if (i - 3 > -1 && chars[i - 1, u] == 'M' && chars[i - 2, u] == 'A' && chars[i - 3, u] == 'S')
+            {
+                counter++;
+            }
+
+            //Vertical Right Downwards
+            if (u + 3 < chars.GetLength(1) && i + 3 < chars.GetLength(0) && chars[i + 1, u + 1] == 'M' && chars[i + 2, u + 2] == 'A' && chars[i + 3, u + 3] == 'S')
+            {
+                counter++;
+            }
+
+            //Vertical Right Upwards
+            if (u + 3 < chars.GetLength(1) && i - 3 > -1 && chars[i - 1, u + 1] == 'M' && chars[i - 2, u + 2] == 'A' && chars[i - 3, u + 3] == 'S')
+            {
+                counter++;
+            }
+
+            //Vertical Left Downwards
+            if (u - 3 > -1 && i - 3 > -1 && chars[i - 1, u - 1] == 'M' && chars[i - 2, u - 2] == 'A' && chars[i - 3, u - 3] == 'S')
+            {
+                counter++;
+            }
+
+            //Vertical Left Upwards
+            if (u - 3 > -1 && i + 3 < chars.GetLength(0) && chars[i + 1, u - 1] == 'M' && chars[i + 2, u - 2] == 'A' && chars[i + 3, u - 3] == 'S')
+            {
+                counter++;
+            }
+
+            return counter;
+        }
+
+        static int CheckFromThatPositionIfWordsExist2(char[,] chars, int i, int u)
+        {
+            int counter = 0;
+
+            if(u - 1 > -1 && i + 1 < chars.GetLength(0) && u + 1 < chars.GetLength(1) && i - 1 > -1)
+            {
+                if ((chars[i - 1, u - 1] == 'M' && chars[i + 1, u + 1] == 'S') || (chars[i - 1, u - 1] == 'S' && chars[i + 1, u + 1] == 'M'))
+                {
+                    if((chars[i + 1, u - 1] == 'M' && chars[i - 1, u + 1] == 'S') || (chars[i + 1, u - 1] == 'S' && chars[i - 1, u + 1] == 'M'))
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+
+        static char[,] BuildCharArray(string[] input)
+        {
+            char[,] chars = new char[input.Length, input[0].Length];
+            for(int i = 0; i < chars.GetLength(0); i++)
+            {
+                for(int u = 0; u < chars.GetLength(1); u++)
+                {
+                    chars[i,u] = input[i][u];
+                }
+            }
+            return chars;
+        }
+    }
+}
