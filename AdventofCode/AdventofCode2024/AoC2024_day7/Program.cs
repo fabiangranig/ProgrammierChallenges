c58324da16bd5f3a5301fc228693a183592324e2
fabiangranig <fabiangranig.fg@gmail.com>
2024-12-08 11:19:03 +0000

    Added AoC2024: day7 and day8

---
+++ AdventofCode/AdventofCode2024/AoC2024_day7/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AoC2024_day7
+{
+    internal class Program
+    {
+        static string[] ComputetPatterns;
+
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            long part1_solution = Part1(input);
+            Console.WriteLine("The solution for part one is: " + part1_solution);
+
+            long part2_solution = Part2(input);
+            Console.WriteLine("The solution for part two is: " + part2_solution);
+        }
+
+        static long Part1(string[] input)
+        {
+            long sum = 0;
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                sum += CheckPossibleOutcome(input[i]);
+            }
+
+            return sum;
+        }
+
+        static long Part2(string[] input)
+        {
+            ComputetPatterns = CreatePatterns2BetterPerformence();
+
+            long sum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += CheckPossibleOutcome2(input[i]);
+            }
+
+            return sum;
+        }
+
+        static long CheckPossibleOutcome(string line)
+        {
+            string[] split = line.Split(':');
+            string[] numbers = RemoveLeadingString(split[1].Split(' '));
+            string[] patterns = CreatePatterns();
+
+            for(int i = 0; i < patterns.Length; i++)
+            {
+                if (patterns[i].Length <= (numbers.Length - 1))
+                {
+                    long betweenSum = CalculateWithPattern(numbers, patterns[i]);
+                    if(Convert.ToString(betweenSum) == split[0])
+                    {
+                        return betweenSum;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        static long CheckPossibleOutcome2(string line)
+        {
+            string[] split = line.Split(':');
+            string[] numbers = RemoveLeadingString(split[1].Split(' '));
+            string[] patterns = ComputetPatterns;
+
+            for (int i = 0; i < patterns.Length; i++)
+            {
+                if (patterns[i].Length <= (numbers.Length - 1))
+                {
+                    long betweenSum = CalculateWithPattern(numbers, patterns[i]);
+                    if (Convert.ToString(betweenSum) == split[0])
+                    {
+                        return betweenSum;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        static long CalculateWithPattern(string[] numbers, string pattern)
+        {
+            while(numbers.Length - 1 != pattern.Length)
+            {
+                pattern = "0" + pattern;
+            }
+
+            long sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(i == 0)
+                {
+                    sum += Int64.Parse(numbers[i]);
+                }
+                else if(pattern[i - 1] == '0')
+                {
+                    sum += Int64.Parse(numbers[i]);
+                }
+                else if (pattern[i - 1] == '1')
+                {
+                    sum *= Int64.Parse(numbers[i]);
+                }
+                else if (pattern[i - 1] == '2')
+                {
+                    sum = Int64.Parse(Convert.ToString(sum) + numbers[i]);
+                }
+            }
+            return sum;
+        }
+
+        static string[] CreatePatterns()
+        {
+            string[] patterns = new string[100000];
+            for(int i = 0; i < patterns.Length; i++)
+            {
+                patterns[i] = Convert.ToString(i, 2);
+            }
+            return patterns;
+        }
+
+        static string[] CreatePatterns2()
+        {
+            string[] patterns = new string[50000];
+            int index = 0;
+            long i = 0;
+            while(index < patterns.Length)
+            {
+                string number = Convert.ToString(i);
+                if(StringConsistsOnlyOfSpecificNumbers(number))
+                {
+                    patterns[index] = number;
+                    index++;
+
+                    if(index % 1000 == 0)
+                    {
+                        Console.WriteLine(index);
+                    }
+                }
+                i++;
+            }
+
+            return patterns;
+        }
+
+        static string[] CreatePatterns2BetterPerformence()
+        {
+            string[] patterns = new string[1000000];
+            patterns[0] = "0";
+            patterns[1] = "1";
+            patterns[2] = "2";
+            int index = 3;
+            int pointer = 0;
+            while (pointer < patterns.Length)
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    if (index >= patterns.Length)
+                    {
+                        break;
+                    }
+
+                    patterns[index] = Convert.ToString(i) + patterns[pointer];
+                    index++;
+                }
+                pointer++;
+            }
+
+            return patterns;
+        }
+
+        static bool StringConsistsOnlyOfSpecificNumbers(string numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(!(numbers[i] == '0' || numbers[i] == '1' || numbers[i] == '2'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static string[] RemoveLeadingString(string[] strings)
+        {
+            string[] newStrings = new string[strings.Length - 1];
+            for(int i = 0; i < newStrings.Length; i++)
+            {
+                newStrings[i] = strings[i + 1];
+            }
+            return newStrings;
+        }
+    }
+}
---
+++ AdventofCode/AdventofCode2024/AoC2024_day8/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024_day8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int part1_solution = Part1(input);
+            Console.WriteLine("The solution to part one is: " + part1_solution);
+
+            int part2_solution = Part2(input);
+            Console.WriteLine("The solution to part two is: " + part2_solution);
+
+        }
+
+        static int Part1(string[] input)
+        {
+            char[,] antenna_map = CreateCharMap(input);
+            char[,] empty_map = CreateEmptyMap(antenna_map.GetLength(0), antenna_map.GetLength(1));
+
+            for(int i = 0; i < antenna_map.GetLength(0); i++)
+            {
+                for(int u = 0; u < antenna_map.GetLength(1); u++)
+                {
+                    empty_map = SearchForPossibleAntinodes(antenna_map, empty_map, i, u);
+                }
+            }
+
+            //Count X on map
+            int counter = 0;
+            for (int i = 0; i < antenna_map.GetLength(0); i++)
+            {
+                for (int u = 0; u < antenna_map.GetLength(1); u++)
+                {
+                    if(empty_map[i, u] == 'X')
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        static int Part2(string[] input)
+        {
+            char[,] antenna_map = CreateCharMap(input);
+            char[,] empty_map = CreateEmptyMap(antenna_map.GetLength(0), antenna_map.GetLength(1));
+
+            for (int i = 0; i < antenna_map.GetLength(0); i++)
+            {
+                for (int u = 0; u < antenna_map.GetLength(1); u++)
+                {
+                    empty_map = SearchForPossibleAntinodes2(antenna_map, empty_map, i, u);
+                }
+            }
+
+            //Count X on map
+            int counter = 0;
+            for (int i = 0; i < antenna_map.GetLength(0); i++)
+            {
+                for (int u = 0; u < antenna_map.GetLength(1); u++)
+                {
+                    if (empty_map[i, u] == 'X')
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        static void OutputMap(char[,] map)
+        {
+            //Output empty map
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int u = 0; u < map.GetLength(1); u++)
+                {
+                    Console.Write(map[i, u]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] SearchForPossibleAntinodes(char[,] antenna_map, char[,] empty_map, int start_y, int start_x)
+        {
+            //Get searched for char
+            char searched = antenna_map[start_y, start_x];
+
+            //Skip if not antenna
+            if(searched == '.')
+            {
+                return empty_map;
+            }
+
+            //Search for the same value from that starting point
+            for(int i = start_y; i < antenna_map.GetLength(0); i++)
+            {
+                for(int u = 0; u < antenna_map.GetLength(1); u++)
+                {
+                    if(i == start_y && u == 0 && start_x + 1 < antenna_map.GetLength(1))
+                    {
+                        u = start_x + 1;
+                    }
+
+                    if(searched == antenna_map[i, u])
+                    {
+                        int difference_y = start_y - i;
+                        int difference_x = start_x - u;
+
+                        int turned_value_y = difference_y * -1;
+                        int turned_value_x = difference_x * -1;
+
+                        int AntinodesFromStart_y = start_y + difference_y;
+                        int AntinodesFromStart_x = start_x + difference_x;
+                        int AntinodesFromSecond_y = i + turned_value_y;
+                        int AntinodesFromSecond_x = u + turned_value_x;
+
+                        if(!(AntinodesFromStart_y < 0 || AntinodesFromStart_y >= empty_map.GetLength(0) || difference_y == 0))
+                        {
+                            if(!(AntinodesFromStart_x < 0 || AntinodesFromStart_x >= empty_map.GetLength(1) || difference_x == 0))
+                            {
+                                empty_map[AntinodesFromStart_y, AntinodesFromStart_x] = 'X';
+                            }
+                        }
+
+                        if (!(AntinodesFromSecond_y < 0 || AntinodesFromSecond_y >= empty_map.GetLength(0) || difference_y == 0))
+                        {
+                            if (!(AntinodesFromSecond_x < 0 || AntinodesFromSecond_x >= empty_map.GetLength(1) || difference_x == 0))
+                            {
+                                empty_map[AntinodesFromSecond_y, AntinodesFromSecond_x] = 'X';
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Return the edited map
+            return empty_map;
+        }
+
+        static char[,] SearchForPossibleAntinodes2(char[,] antenna_map, char[,] empty_map, int start_y, int start_x)
+        {
+            //Get searched for char
+            char searched = antenna_map[start_y, start_x];
+
+            //Skip if not antenna
+            if (searched == '.')
+            {
+                return empty_map;
+            }
+
+            //Search for the same value from that starting point
+            for (int i = start_y; i < antenna_map.GetLength(0); i++)
+            {
+                for (int u = 0; u < antenna_map.GetLength(1); u++)
+                {
+                    if (i == start_y && u == 0 && start_x + 1 < antenna_map.GetLength(1))
+                    {
+                        u = start_x + 1;
+                    }
+
+                    if (searched == antenna_map[i, u])
+                    {
+                        empty_map[i, u] = 'X';
+                        empty_map[start_y, start_x] = 'X';
+
+                        int difference_y = start_y - i;
+                        int difference_x = start_x - u;
+
+                        int turned_value_y = difference_y * -1;
+                        int turned_value_x = difference_x * -1;
+
+                        int AntinodesFromStart_y = start_y + difference_y;
+                        int AntinodesFromStart_x = start_x + difference_x;
+                        int AntinodesFromSecond_y = i + turned_value_y;
+                        int AntinodesFromSecond_x = u + turned_value_x;
+                        while (1 == 1)
+                        {
+                            if (!(AntinodesFromStart_y < 0 || AntinodesFromStart_y >= empty_map.GetLength(0) || difference_y == 0))
+                            {
+                                if (!(AntinodesFromStart_x < 0 || AntinodesFromStart_x >= empty_map.GetLength(1) || difference_x == 0))
+                                {
+                                    empty_map[AntinodesFromStart_y, AntinodesFromStart_x] = 'X';
+                                }
+                            }
+
+                            if(AntinodesFromStart_y >= empty_map.GetLength(0) || AntinodesFromStart_y < 0 || difference_y == 0)
+                            {
+                                break;
+                            }
+                            if (AntinodesFromStart_x >= empty_map.GetLength(1) || AntinodesFromStart_x < 0 || difference_x == 0)
+                            {
+                                break;
+                            }
+
+                            AntinodesFromStart_y += difference_y;
+                            AntinodesFromStart_x += difference_x;
+                        }
+
+                        while(1 == 1)
+                        {
+                            if (!(AntinodesFromSecond_y < 0 || AntinodesFromSecond_y >= empty_map.GetLength(0) || difference_y == 0))
+                            {
+                                if (!(AntinodesFromSecond_x < 0 || AntinodesFromSecond_x >= empty_map.GetLength(1) || difference_x == 0))
+                                {
+                                    empty_map[AntinodesFromSecond_y, AntinodesFromSecond_x] = 'X';
+                                }
+                            }
+
+                            if(AntinodesFromSecond_y < 0 || AntinodesFromSecond_y >= empty_map.GetLength(0) || difference_y == 0)
+                            {
+                                break;
+                            }
+                            if(AntinodesFromSecond_x < 0 || AntinodesFromSecond_x >= empty_map.GetLength(1) || difference_x == 0)
+                            {
+                                break;
+                            }
+
+                            AntinodesFromSecond_y += turned_value_y;
+                            AntinodesFromSecond_x += turned_value_x;
+                        }
+                    }
+                }
+            }
+
+            //Return the edited map
+            return empty_map;
+        }
+
+        static char[,] CreateCharMap(string[] input)
+        {
+            char[,] chars = new char[input.Length, input[0].Length];
+            for(int i = 0; i < chars.GetLength(0); i++)
+            {
+                for(int u = 0; u < chars.GetLength(1); u++)
+                {
+                    chars[i, u] = input[i][u];
+                }
+            }
+            return chars;
+        }
+
+        static char[,] CreateEmptyMap(int length_1, int length_2)
+        {
+            char[,] chars = new char[length_1, length_2];
+            for (int i = 0; i < chars.GetLength(0); i++)
+            {
+                for (int u = 0; u < chars.GetLength(1); u++)
+                {
+                    chars[i, u] = '0';
+                }
+            }
+            return chars;
+        }
+    }
+}
